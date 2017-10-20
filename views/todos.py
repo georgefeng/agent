78dc81e1815f499cfba29929496570e81bb3620c
KN_Feng <edu_feng@163.com>
2017-10-20 17:20:38 +0800

    update [todos]: Add schedule.

--- views/todos.py
+++ views/todos.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import datetime
 
 from leancloud import Object
 from leancloud import Query
@@ -37,3 +38,76 @@ def add():
     except LeanCloudError as e:
         return e.error, 502
     return redirect(url_for('todos.show'))
+
+@todos_view.route('/schedule', methods=['GET'])
+def show_schedule():
+    todos = Query(Todo).descending('createdAt').find()
+    
+    action_list = []
+    for a_todo in todos:
+        action_list.append(eval(a_todo.get('content')))
+
+    task_list = []
+
+    for _action in action_list:
+        for _time, _text in _action.items(): 
+            if 'add_task' in _text:
+                _freq = int(_text[10])
+                _task_name = str(_text[12:])
+
+                task_list.append(
+                    {
+                        'task_name': _task_name,
+                        'freq': _freq,
+                        'last': _time
+                    }
+                )
+
+            elif 'exec_task' in _text:
+                _task_name = str(_text[11:])
+                _task_id = [_id for _id, _dict in enumerate(task_list) if _dict['task_name'] == _task_name][0]
+                task_list[_task_id]['last'] = _time
+    
+
+    todo_list = []
+    for _task in task_list:
+        # now
+        _now = datetime.datetime.now()
+
+        # num2datetime
+        _str_date = str(_task['last'])
+        _py_date = datetime.datetime.strptime(_str_date,'%Y%m%d%H%M%S')
+
+        # diff_time
+        _diff = (_now - _py_date).total_seconds()
+
+        # heat
+        _task_freq = int(_task['freq'])
+        if _task_freq == 0:
+            _heat = round(_diff * 0.5, 2)
+
+        elif _task_freq == 1:
+            _heat = round(_diff/60 - 1, 2)
+        elif _task_freq == 2:
+            _heat = round(_diff/60/60 - 1, 2)
+        elif _task_freq == 3:
+            _heat = round(_diff/60/60/24 - 1, 2)
+        elif _task_freq == 4:
+            _heat = round(_diff/60/60/24/7 - 1, 2)
+        elif _task_freq == 5:
+            _heat = round(_diff/60/60/24/30 - 1, 2)
+        elif _task_freq == 6:
+            _heat = round(_diff/60/60/24/365 - 1, 2)
+        else:
+            _heat = -9
+
+        # update todo_list
+        todo_list.append([_heat, _task['freq'], _task['task_name']])
+
+        # print todo list
+        todo_list.sort(key=lambda x:x[0],reverse=True)  #逆序
+        todo_list[0:0] = [['heat', 'freq', 'task_name']]
+
+        return str(todo_list)
+
+
